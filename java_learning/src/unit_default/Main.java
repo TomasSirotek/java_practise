2636ffdb8057da04af1d027947225a30a4be7a31
TomassSimko <simko.t@email.cz>
2022-09-06 18:21:42 +0200

    scanner playing

--- java_learning/src/unit_default/Main.java
+++ java_learning/src/unit_default/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
        System.out.println("Default playground for unit 0")
+       Scanner scanner = new Scanner(System.in);
+       int numberInput = scanner.nextInt();
+       System.out.println(numberInput);
     }
 }
