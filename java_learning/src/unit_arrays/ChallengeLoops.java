392905c9572074484985712018eedcf5c8512e2c
TomassSimko <simko.t@email.cz>
2022-09-12 16:52:09 +0200

    finished with arrays

--- java_learning/src/unit_arrays/ChallengeLoops.java
+++ java_learning/src/unit_arrays/ChallengeLoops.java
@@ -6,43 +6,56 @@ public class ChallengeLoops {
     public static void main(String[] args) {
         // TODO: Create an integer array of 10 elements and give each element an arbitrary value. Print out the entire array using a loop.
 
-//        int[] numbers = {1,2,3,4,5,6,7,8,9,10};
-//        for (int i: numbers) {
-//            System.out.println(i);
-//        }
+        int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+        for (int i: numbers) {
+            System.out.println(i);
+        }
 
         // TODO : Use a loop to populate an array with integers from 1 to 20. Use another loop to print the contents of the array (the elements) backwards.
 
-//        int[] numbers2 = new int[20]; // set to 20 positions
-//        for (int i = 0; i < numbers2.length; i++) {
-//           numbers2[i] = i + 1;
-//        }
-//
-//        for (int t: numbers2
-//             ) {
-//            System.out.println(t);
-//        }
+        int[] numbers2 = new int[20]; // set to 20 positions
+        for (int i = 0; i < numbers2.length; i++) {
+           numbers2[i] = i + 1;
+        }
+
+        for (int t: numbers2
+             ) {
+            System.out.println(t);
+        }
 
         // TODO :
         // Create an array of five doubles and give each element some value. Calculate the sum of all the elements and print the result. Calculate the mean/average of the all the elements and print that too.
         
-//        double[] doubles = new double[]{33.5, 23.5, 56.2, 5.3, 2.4};
-//        double totalSum = 0;
-//        int mean = 0;
-//        for (int i = 0; i < doubles.length; i++) {
-//            totalSum += doubles[i];
-//        }
-//
-//        mean = (int)totalSum / doubles.length;
-//
-//        System.out.println(mean);
-//        System.out.println(totalSum);
+        double[] doubles = new double[]{33.5, 23.5, 56.2, 5.3, 2.4};
+        double totalSum = 0;
+        int mean = 0;
+        for (int i = 0; i < doubles.length; i++) {
+            totalSum += doubles[i];
+        }
+
+        mean = (int)totalSum / doubles.length;
+
+        System.out.println(mean);
+        System.out.println(totalSum);
 
         // TODO : Write a program which asks the user to enter their top five favourite foods and store their answers in a string array.
         //  However, create the array so it can only hold a maximum of three strings. After the user enters the third item, print “No more memory available.”,
         //  then print out the contents of the array.
 
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Top 5 foods ?");
+        String[] foodsArray = new String[3];
 
+        for (int i = 0; i < foodsArray.length; i++) {
+            System.out.println("Food " + (i + 1) + "?" );
+            foodsArray[i] = scanner.nextLine();
+        }
+        System.out.println("Out of memory");
+        System.out.println("Favourite foods are top 3 => ");
+
+        for (String s: foodsArray
+        ) {
+            System.out.println(s);
+        }
     }
 }
