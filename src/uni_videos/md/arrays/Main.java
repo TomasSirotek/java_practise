414f5cb87962fc202c12773e01cee56fcd5ba03f
TomassSimko <simko.t@email.cz>
2022-10-06 16:17:26 +0200

    practise on 3d arrays with object

--- src/uni_videos/md/arrays/Main.java
+++ src/uni_videos/md/arrays/Main.java
@@ -33,8 +33,49 @@ public class Main {
 
         // 3d
 
+        int[][][] _3dArray = new int[3][3][3];
+
+        _3dArray[0][0][0] = 10;
+        _3dArray[1][1][0] = 20;
+        _3dArray[2][0][0] = 30;
+
+        for (int r = 0; r < _3dArray.length; r++) {
+            for (int r2 = 0; r2 < _3dArray[r].length; r2++) {
+                for (int c = 0; c < _3dArray[r2].length; c++) {
+                    System.out.print(_3dArray[r][r2][c] + "" + " , ");
+                }
+                System.out.println();
+            }
+        }
+
+        User[][][] _userArray = new User[2][3][30];
+
+        _userArray[0][0][0] = new User(1,"User 1","email");
+        _userArray[0][1][0] = new User(2,"User 2","email2");
+        _userArray[0][2][0] = new User(2,"User 2","email2");
+
+        _userArray[1][0][0] = new User(3,"User 3","email3");
+        for (int r = 0; r < _userArray.length; r++) {
+            for (int r2 = 0; r2 < _userArray[r].length; r2++) {
+                for (int c = 0; c < _3dArray[r2].length; c++) {
+                    System.out.print(_3dArray[r][r2][c] + "" + " , ");
+                }
+                System.out.println();
+            }
+        }
 
 
+    }
 
+    public static class User {
+        public int id;
+        public String name;
+        public String email;
+
+        public User(int id,String name,String email){
+            this.id = id;
+            this.name = name;
+            this.email = email;
+        }
     }
 }
