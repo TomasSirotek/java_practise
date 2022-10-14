644e041ef74edd94bd8daaffa81762aee3523b43
TomassSimko <simko.t@email.cz>
2022-10-14 23:09:52 +0200

    play with 3d arrays

--- src/polymor_basics/Main.java
+++ src/polymor_basics/Main.java
@@ -6,15 +6,16 @@ import polymor_basics.model.Dog;
 
 public class Main {
     public static void main(String[] args) {
+         // POLYMORPHISM - METHOD OVERLOADING WHEN SAME METHOD
+         // NAME DOES DIFFERENT THING AND HAVE DIFFERENT PARAMS
 
          Animal a = new Animal();
          a.eat();
-
+          // eating
 
          Dog d = new Dog("Dick");
          d.eat();
         // System.out.println("Breed of the dog is " + d.getBreed());
-
          Cat c = new Cat();
          c.setIsHungry(true);
          c.eat();
@@ -22,6 +23,60 @@ public class Main {
          // Method overloading  same method with different params
          d.eat(2);
 
-         System.out.println(c.getIsHungry());
+        //  System.out.println(c.getIsHungry());
+
+         String[][] field = new String[2][2];
+
+         for (int r = 0; r < field.length; r++) {
+              for (int col = 0; col < field.length; col++) {
+                    field[r][col] = " Empty " + " ";
+              }
+         }
+
+         for (int i = 0; i < field.length; i++) {
+              for (int j = 0; j < field.length; j++) {
+                   System.out.print(field[i][j]);
+              }
+              System.out.println();
+         }
+
+         String[][][] _field3d = new String[3][3][3];
+         int currentState = 0;
+         String textOutPut = " X " + " ";
+         String textOutPut2 = " 0 " + " ";
+
+
+         for (int r = 0; r < _field3d.length; r++) {
+              for (int col = 0; col < _field3d.length; col++) {
+                   for (int in = 0; in < _field3d.length; in++) {
+                       _field3d[r][col][in] = currentState == 0 ? textOutPut : textOutPut2;
+                       if(currentState == 0){
+                            currentState = 1;
+                       } else
+                            currentState = 0;
+                   }
+              }
+         }
+
+         for (int i = 0; i < _field3d.length; i++) {
+              for (int j = 0; j < _field3d.length; j++) {
+                   for (int k = 0; k < _field3d.length; k++) {
+                        System.out.print(_field3d[i][j][k]);
+                   }
+                   System.out.println();
+              }
+         }
+
+
+
+    }
+
+    private static int setState(int currentState){
+         if(currentState == 0)
+              currentState = 1;
+         else
+              currentState = 0;
+
+         return currentState;
     }
 }
