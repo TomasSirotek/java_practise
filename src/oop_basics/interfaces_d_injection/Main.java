d959ccb31692e88d073299af96ad6a3c9db0d5cc
TomassSimko <simko.t@email.cz>
2022-10-31 14:20:58 +0100

    cart shop test interfaces

--- src/oop_basics/interfaces_d_injection/Main.java
+++ src/oop_basics/interfaces_d_injection/Main.java
@@ -2,7 +2,9 @@ package oop_basics.interfaces_d_injection;
 
 public class Main {
     public static void main(String[] args) {
+
         ICart cart = new Cart();
         CoreShop coreShop = new CoreShop(cart);
+        CoreShop coreShop2 = new CoreShop(cart);
     }
 }
