3355d195fad712ddd00ba87307e51ab392fe2086
TomassSimko <simko.t@email.cz>
2022-10-31 14:21:49 +0100

    not working at the moment testing

--- src/oop_basics/interfaces_d_injection/Main.java
+++ src/oop_basics/interfaces_d_injection/Main.java
@@ -2,9 +2,7 @@ package oop_basics.interfaces_d_injection;
 
 public class Main {
     public static void main(String[] args) {
-
         ICart cart = new Cart();
         CoreShop coreShop = new CoreShop(cart);
-        CoreShop coreShop2 = new CoreShop(cart);
     }
 }
