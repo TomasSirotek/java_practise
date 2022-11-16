7a7bcb0b0418c4502d9684f72ed9bd8c24aa010d
TomassSimko <simko.t@email.cz>
2022-11-16 13:57:07 +0100

    y

--- src/oop_basics/file_handling/Main.java
+++ src/oop_basics/file_handling/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
         printFileContents();
-        // addToFile("Suck me jin jang");
+         ddToFile("Suck me jin jang");
         addTextAtLine("Testik",2);
         printFileContents();
     }
