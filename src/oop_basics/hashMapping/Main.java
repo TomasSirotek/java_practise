0635a4beeda85c1a43e2868ca8237a4adf0b0e3b
TomassSimko <simko.t@email.cz>
2022-11-23 21:10:29 +0100

    play with hashmaps

---
+++ src/oop_basics/hashMapping/Main.java
@@ -0,0 +1,10 @@
+package oop_basics.hashMapping;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<Integer,String> idWithName = new HashMap<>();
+
+    }
+}
