314780700693808a2026a02c2205f81c6cb4bef8
TomassSimko <simko.t@email.cz>
2022-12-09 14:22:40 +0100

    more abstraction

--- src/abstraction_basics_two/o/AbstractCLASS.java
+++ src/abstraction_basics_two/o/AbstractCLASS.java
@@ -1,7 +1,11 @@
 package abstraction_basics_two.o;
 
 public abstract class AbstractCLASS {
+
     // we can have both methods and abstract methods
     public abstract String getStringValue();
 
+    public AbstractCLASS(){
+
+    }
 }
