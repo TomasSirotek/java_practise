7a34b1522bfba0321b0cd9633f04b620a400464d
TomassSimko <simko.t@email.cz>
2022-12-17 14:40:13 +0100

    .

--- src/oop_basics/hashsets/Main.java
+++ src/oop_basics/hashsets/Main.java
@@ -2,6 +2,11 @@ package oop_basics.hashsets;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("asda");
+        System.out.println("asda");
+        System.out.println("asda");
+        System.out.println("asda");
+        System.out.println("asda");
 
-    }
+s    }
 }
