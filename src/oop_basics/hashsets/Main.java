ed5faa2683872357c4ff03e16f09c32940876a46
TomassSimko <simko.t@email.cz>
2022-12-25 12:54:24 +0100

    .

--- src/oop_basics/hashsets/Main.java
+++ src/oop_basics/hashsets/Main.java
@@ -1,5 +1,9 @@
 package oop_basics.hashsets;
 
+import oop_basics.dao_pattern.Role;
+
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("asda");
@@ -7,6 +11,10 @@ public class Main {
         System.out.println("asda");
         System.out.println("asda");
         System.out.println("asda");
+
+        HashMap<Integer,Role> roleHashMap = new HashMap<>();
+
+
 
    }
 }
