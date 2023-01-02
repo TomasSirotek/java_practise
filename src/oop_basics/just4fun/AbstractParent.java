08855c614d40b4fc86592976fffc31e4b708b76b
TomassSimko <simko.t@email.cz>
2023-01-02 22:04:00 +0100

    .

--- src/oop_basics/just4fun/AbstractParent.java
+++ src/oop_basics/just4fun/AbstractParent.java
@@ -3,4 +3,5 @@ package oop_basics.just4fun;
 public abstract class AbstractParent implements AbstractInterface{
 
     public abstract void refresh();
+    // https://moodle.easv.dk/mod/assign/view.php?id=99295
 }
