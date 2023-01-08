ddcfdbed1648caacef4d699534d670beff2f198a
TomassSimko <simko.t@email.cz>
2023-01-08 16:53:31 +0100

    .

--- src/oop_basics/better_dep_injection/Car.java
+++ src/oop_basics/better_dep_injection/Car.java
@@ -1,6 +1,6 @@
 package oop_basics.better_dep_injection;
 
-public record Car(String name) {
+public record Car(String name,int id) {
     @Override
     public String toString() {
         return "This car has name: " + name;
