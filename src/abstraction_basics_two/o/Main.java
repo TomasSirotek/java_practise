6ea71fe21c1057a3e954ce7c7c562c2a5a91d0ee
TomassSimko <simko.t@email.cz>
2023-01-30 17:30:55 +0100

    list iter

--- src/abstraction_basics_two/o/Main.java
+++ src/abstraction_basics_two/o/Main.java
@@ -1,7 +1,10 @@
 package abstraction_basics_two.o;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        var list = List.of("$0","$1");
+        list.forEach(System.out::println);
     }
 }
