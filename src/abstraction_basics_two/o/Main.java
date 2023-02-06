fa803e7a1f08c53e19c4a6c1c43e566f634e5d4a
TomassSimko <simko.t@email.cz>
2023-02-06 10:33:12 +0100

    .

--- src/abstraction_basics_two/o/Main.java
+++ src/abstraction_basics_two/o/Main.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        var list = List.of("$0","$1");
+        var list = List.of("$0","$1","$2");
         list.forEach(System.out::println);
         list.forEach(System.out::println);
     }
